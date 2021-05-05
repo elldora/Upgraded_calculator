c78eaf2d77a0b80d5cbd1aa1b40caf8c82f893cc
elahe dorani <elahe_2rani@yahoo.com>
2021-05-05 15:56:51 +0430

    upgraded to use nullable reference types

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+
+#nullable enable
 
 namespace calculator
 {
@@ -12,43 +15,47 @@ namespace calculator
         private static void ask_number_one()
         {
             Console.Write("Enter any number >> ");
-            string input = Console.ReadLine();
-            float number = 0;
-
-            if (float.TryParse(input, out number))
+            string? input = Console.ReadLine();
+			
+			//target-typed expressions introduced in C# 9.0 ...
+            float? number_one = (float.TryParse(input, out float number)) ? number : null;
+			
+            if (number_one != null)
             {
-                ask_number_two(number);
+				ask_number_two(number_one);
             }
             else
             {
                 Console.WriteLine("Emphasis on number.");
                 ask_number_one();
             }
+			
         }
 
-        private static void ask_number_two(float number_one)
+        private static void ask_number_two(float? number_one)
         {
             Console.Write("Enter a second number >> ");
-            string input = Console.ReadLine();
-            float number = 0;
+            string? input = Console.ReadLine();
+            float? number_two = (float.TryParse(input, out float number)) ? number : null;
 
-            if (float.TryParse(input, out number))
+            if (number_two != null)
             {
-                ask_operation(number_one, number);
+                ask_operation(number_one, number_two);
             }
-            else
+            else 
             {
                 Console.WriteLine("Emphasis on number.");
                 ask_number_two(number_one);
             }
         }
 
-        private static void ask_operation(float number_one, float number_two)
+        private static void ask_operation(float? number_one, float? number_two)
         {
             Console.Write("Enter any operator (add, +, sub, -, mul, *, div, /) >> ");
-            string input = Console.ReadLine();
-            switch (input)
-            {
+            string? input = Console.ReadLine();
+            
+			switch (input!)
+			{
                 case "add":
                 case "+":
                     Console.WriteLine("{0} plus {1} is {2}", number_one, number_two, (number_one + number_two));
@@ -81,11 +88,16 @@ namespace calculator
             Console.Write("Do you want to play again? (y/n) >> ");
             string input = Console.ReadLine();
 
-            if (input.ToLower() == "y")
-            {
-                Console.Clear();
-                ask_number_one();
-            }   
+			if (input.ToLower() == "y")
+			{
+				Console.Clear();
+				ask_number_one();
+			} 
+			
+			else
+			{
+				Console.Clear();
+			}
         }
     }
 }
